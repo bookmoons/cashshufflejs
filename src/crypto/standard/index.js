e3026bcb916646a818cfcb989cf016dd37fc9ece
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-07-16 07:15:36 -0400

    crypto/std: Link to interface

--- src/crypto/standard/index.js
+++ src/crypto/standard/index.js
@@ -1,5 +1,6 @@
 /**
- * Standard implementation of message encryption.
+ * Standard implementation of
+ *     {@link module:cashshuffle/crypto~Crypto|message encryption}.
  *
  * Uses [jssha](https://www.npmjs.com/package/jssha) for hashing.
  * @module cashshuffle/crypto/standard
