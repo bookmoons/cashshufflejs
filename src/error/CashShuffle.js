3f4a0d9da067abd29741231d271d50fad412515e
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-07-16 06:07:31 -0400

    doc: Combine error module

--- src/error/CashShuffle.js
+++ src/error/CashShuffle.js
@@ -1,10 +1,15 @@
-/**
- * Base cashshuffle error.
- * @module cashshuffle/error/CashShuffle
- */
-
 import { VError } from 'verror'
 
+/**
+ * Base class for cashshuffle errors.
+ *
+ * You can detect all cashshuffle errors with `e instanceof CashShuffleError`.
+ *
+ * @memberof module:cashshuffle/error
+ * @extends module:verror.VError
+ * @see {@link https://www.npmjs.com/package/verror#constructors}
+ *     for constructor parameters.
+ */
 class CashShuffleError extends VError {
   get name () {
     return 'CashShuffleError'
