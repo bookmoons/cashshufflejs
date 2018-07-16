f32f2fb4530b76e12634f360280926cb0d245ab4
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-07-16 05:07:47 -0400

    test: Move hash to single file

--- test/unit/crypto/hash.js
+++ test/unit/crypto/hash.js
@@ -6,7 +6,7 @@ const text = 'The quick brown fox jumps over the lazy dog.'
 const correctHashHex =
     '619cba8e8e05826e9b8c519c0a5c68f4fb653e8a3d8aa04bb2c8cd4c'
 
-test('hash text', t => {
+test('text', t => {
   const textHash = hash(text)
   const textHashHex = arrayBufferToHex(textHash)
   t.true(textHashHex === correctHashHex)
