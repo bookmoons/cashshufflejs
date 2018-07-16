2ca669ca0251b0f07176238fd2a43ce36aea4ac9
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-07-16 07:13:12 -0400

    crypto/standard: Start docs

--- src/crypto/standard/hash.js
+++ src/crypto/standard/hash.js
@@ -1,13 +1,5 @@
 import Hasher from 'jssha'
 
-/**
- * Hash a string with SHA-224.
- * @memberof module:cashshufflejs/crypto.Crypto
- *
- * @param {string} text - Text to hash.
- *
- * @return {ArrayBuffer} SHA-224 hash of the text.
- */
 function hash (text) {
   const hasher = new Hasher('SHA-224', 'TEXT')
   hasher.update(text)
---
+++ src/crypto/standard/index.js
@@ -0,0 +1,15 @@
+/**
+ * Standard implementation of message encryption.
+ *
+ * Uses [jssha](https://www.npmjs.com/package/jssha) for hashing.
+ * @module cashshuffle/crypto/standard
+ */
+
+import Crypto from './main'
+import hash from './hash'
+
+Object.assign(Crypto, {
+  hash
+})
+
+export default Crypto
