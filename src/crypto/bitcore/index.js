1a5c1ae1da3a9b7c24dbe6379d5dd7a7b1437cdf
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-07-30 15:47:32 -0400

    crypto/bc: Document @bookmoons/bitcore-ecies-cash usage

--- src/crypto/bitcore/index.js
+++ src/crypto/bitcore/index.js
@@ -5,6 +5,7 @@
  * Uses:
  * - [crypto][2] for hashing.
  * - [bitcore-lib-cash][3] for key operations.
+ * - [@bookmoons/bitcore-ecies-cash][4] for encryption operations.
  *
  * Network values are bitcore-lib-cash `Network` instances. Standard networks
  * are available through `bitcore.Networks`.
@@ -12,6 +13,7 @@
  * [1]: https://bitcore.io/
  * [2]: https://nodejs.org/dist/latest/docs/api/crypto.html
  * [3]: https://www.npmjs.com/package/bitcore-lib-cash
+ * [4]: https://www.npmjs.com/package/@bookmoons/bitcore-ecies-cash
  *
  * @module cashshuffle/crypto/bitcore
  */
