70108f79e518eacdaae733d40ba92352c6766a29
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-12 02:19:41 -0400

    protocol: Add package index

---
+++ src/protocol/index.js
@@ -0,0 +1,10 @@
+/**
+ * Protocol details. Message types with [protobuf][1].
+ *
+ * Messages on the wire are terminated with the Unicode character U+23CE
+ * Return Symbol in UTF-8. The byte encoding is e2 8f 8e.
+ *
+ * [1]: https://developers.google.com/protocol-buffers/
+ *
+ * @module cashshuffle/protocol
+ */
--- src/protocol/type.js
+++ src/protocol/type.js
@@ -1,13 +1,4 @@
-/**
- * Protocol data types. Defined with [protobuf][1].
- *
- * Messages on the wire are terminated with the Unicode character U+23CE
- * Return Symbol in UTF-8. The byte encoding is e2 8f 8e.
- *
- * [1]: https://developers.google.com/protocol-buffers/
- *
- * @module cashshuffle/protocol
- */
+/* Protocol types. */
 
 /**
  * Phase enumeration.
