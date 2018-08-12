f144b890048dea9d3e00ba29aa041589d63fe854
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-12 01:18:24 -0400

    error: Expose BusyError

--- src/error/index.js
+++ src/error/index.js
@@ -6,5 +6,6 @@
  */
 
 export { default as CashShuffleError } from './CashShuffle'
+export { default as BusyError } from './Busy'
 export { default as MissingValueError } from './MissingValue'
 export { default as NotImplementedError } from './NotImplemented'
--- test/com/error/package.js
+++ test/com/error/package.js
@@ -1,11 +1,16 @@
 import test from 'ava'
 import CashShuffleError from 'error/CashShuffle'
+import BusyError from 'error/Busy'
 import MissingValueError from 'error/MissingValue'
 import NotImplementedError from 'error/NotImplemented'
 import * as error from 'error'
 
 test('CashShuffle', t => {
   t.is(error.CashShuffleError, CashShuffleError)
+})
+
+test('Busy', t => {
+  t.is(error.BusyError, BusyError)
 })
 
 test('MissingValue', t => {
