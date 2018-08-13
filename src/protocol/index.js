30f08a38eb468c0632340881999132d3d474341c
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-12 22:19:19 -0400

    protocol: Add message terminator constants

--- src/protocol/index.js
+++ src/protocol/index.js
@@ -8,3 +8,51 @@
  *
  * @module cashshuffle/protocol
  */
+
+/**
+ * Length in characters of message terminator.
+ *
+ * @constant {number}
+ * @default 1
+ */
+const terminatorCharLength = 1
+
+/**
+ * Length in bytes of message terminator.
+ *
+ * @constant {number}
+ * @default 3
+ */
+const terminatorByteLength = 3
+
+/**
+ * Message terminator as a string.
+ *
+ * @constant {string}
+ * @default '\u23ce'
+ */
+const terminatorString = '\u23ce'
+
+/**
+ * Message terminator in UTF-8 in a Node.js `Buffer`.
+ *
+ * @constant {Buffer}
+ * @default [ 0xe2, 0x8f, 0x8e ]
+ */
+const terminatorBuffer = Buffer.from([ 0xe2, 0x8f, 0x8e ])
+
+/**
+ * Message terminator in UTF-8 in an `ArrayBuffer`.
+ *
+ * @constant {ArrayBuffer}
+ * @default [ 0xe2, 0x8f, 0x8e ]
+ */
+const terminatorBinary = Uint8Array.from([ 0xe2, 0x8f, 0x8e ]).buffer
+
+export {
+  terminatorCharLength,
+  terminatorByteLength,
+  terminatorString,
+  terminatorBuffer,
+  terminatorBinary
+}
