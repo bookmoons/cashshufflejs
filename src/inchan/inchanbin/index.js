66267fce172064797698ec910d42f838fe2ffb9d
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-13 02:34:38 -0400

    inchan/icb: Correct comment

--- src/inchan/inchanbin/index.js
+++ src/inchan/inchanbin/index.js
@@ -2,7 +2,7 @@
  * Inchanbin based implementation of Inchan.
  *
  * Uses an `Inchanbin` instance to receive raw binary messages.
-
+ *
  * Provide `Inchanbin` instance and a `protobuf.Root` object for the
  * CashShuffle protocol to the constructor.
  *
