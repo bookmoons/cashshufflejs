298d717fd3e723a89f5869479cee73e83f7b53fa
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-13 02:42:23 -0400

    inchan/icb: Link to protobufjs.Root

--- src/inchan/inchanbin/index.js
+++ src/inchan/inchanbin/index.js
@@ -3,8 +3,10 @@
  *
  * Uses an `Inchanbin` instance to receive raw binary messages.
  *
- * Provide `Inchanbin` instance and a `protobufjs.Root` object for the
+ * Provide `Inchanbin` instance and a [protobufjs.Root][1] object for the
  * CashShuffle protocol to the constructor.
+ *
+ * [1]: https://dcode.io/protobuf.js/Root.html
  *
  * @module cashshuffle/inchan/inchanbin
  */
