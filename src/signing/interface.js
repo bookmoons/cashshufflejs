c6f3e46f5954fb46c931180ccca3628761f7ab33
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-16 01:10:54 -0400

    signing: Define interface skeleton

---
+++ src/signing/interface.js
@@ -0,0 +1,16 @@
+/**
+ * Message signing.
+ * @module cashshuffle/signing
+ */
+
+/**
+ * Message signing functions.
+ *
+ * Supports key generation, signing, and verifying.
+ *
+ * Performs [ECDSA][1] digital signature operations.
+ *
+ * [1]: https://cryptopp.com/wiki/Elliptic_Curve_Digital_Signature_Algorithm
+ *
+ * @interface Signing
+ */
