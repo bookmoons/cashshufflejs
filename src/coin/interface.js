54479f014d51dfad7bc155b8796a6dcec9bdf19b
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-17 05:43:51 -0400

    coin: Define #makeUnsignedTransaction

--- src/coin/interface.js
+++ src/coin/interface.js
@@ -10,6 +10,37 @@
  */
 
 /**
+ * Participant address pair.
+ *
+ * Associates a Bitcoin Cash address to a named participant.
+ * 2 element array.
+ *
+ * @typedef {Array<string>} ParticipantAddress
+ * @memberof module:cashshuffle/coin~Coin
+ *
+ * @prop {string} 1 - Participant session public key as hex string.
+ * @prop {string} 2 - Bitcoin Cash address.
+ *
+ * @example
+const sessionPublicKey =
+  '020db431245713add097421a29ec3089f01587a3808d1043fee5956fc5e08effcd'
+const address = 'bitcoincash:qr975e2q784jnk0pq2rrk9enuywttyhxryfkyuyjq3'
+const participantAddress = [ sessionPublicKey, address ]
+ */
+
+/**
+ * Bitcoin Cash transaction.
+ *
+ * Used by various methods to construct a multinput multioutput
+ * multisignature transaction over several calls.
+ *
+ * Type and usage details implementation defined.
+ *
+ * @typedef Transaction
+ * @memberof module:cashshuffle/coin~Coin
+ */
+
+/**
  * Get P2PKH address for public key.
  *
  * @method address
@@ -20,6 +51,32 @@
  * @param {string} publicKeyString - Public key as hex string.
  *
  * @return {string} P2PKH address for the public key.
+ */
+
+/**
+ * Make unsigned transaction.
+ *
+ * `Map` instances work well for `inputs` and `changes`.
+ * `Set` instances work well for `outputs`.
+ *
+ * @method makeUnsignedTransaction
+ * @memberof module:cashshuffle/coin~Coin
+ * @instance
+ * @async
+ *
+ * @param {number} amount - Participant transaction amount in satoshis.
+ *     The produced transaction will transfer this amount for each participant.
+ * @param {number} fee - Participant fee amount in satoshis.
+ *     The produced transaction will charge this fee to each participant.
+ * @param {Iterable<ParticipantAddress>} inputs - Transaction inputs.
+ *     1 for each participant.
+ * @param {Iterable<string>} outputs - Transaction outputs. Each item is a
+ *     Bitcoin Cash address. 1 for each participant. Participant associated
+ *     with each output unspecified.
+ * @param {Iterable<ParticipantAddress>} changes - Transaction change outputs.
+ *     0-1 for each participant.
+ *
+ * @return {Transaction} The unsigned transaction.
  */
 
 /**
