e7c1246ac9552b71db27cd765a07a631507ab54a
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-17 02:34:50 -0400

    coin: Define #verifySignature

--- src/coin/interface.js
+++ src/coin/interface.js
@@ -21,3 +21,19 @@
  *
  * @return {string} P2PKH address for the public key.
  */
+
+/**
+ * Verify message signature.
+ *
+ * @method verifySignature
+ * @memberof module:cashshuffle/coin~Coin
+ * @instance
+ * @async
+ *
+ * @param {string} signature - Message signature as Base64 encoded string.
+ * @param {string} message - Plaintext message.
+ * @param {string} publicKeyString - Signer public key as hex string.
+ *
+ * @return {boolean} Whether the signature is valid. True if from the named
+ *     signer and for the given message.
+ */
