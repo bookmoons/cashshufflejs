6d3ba08065ae36a7b85db21d2c8a4c23b4089626
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-22 08:58:09 -0400

    Define Agent

---
+++ src/agent/interface.js
@@ -0,0 +1,24 @@
+/**
+ * Message agent.
+ * @module cashshuffle/agent
+ */
+
+/**
+ * Message agent.
+ *
+ * Receives protocol messages.
+ * Responds to messages in an implementation defined way.
+ *
+ * @interface Agent
+ */
+
+/**
+ * Submit message.
+ *
+ * @method submit
+ * @memberof module:cashshuffle/agent~Agent
+ * @instance
+ * @async
+ *
+ * @param {*} message - Message to submit.
+ */
