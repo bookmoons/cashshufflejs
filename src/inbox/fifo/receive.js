31fce4514bce02b8069fde00be82deae29fe0120
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-22 08:45:47 -0400

    inbox/fifo: Add #receive

---
+++ src/inbox/fifo/receive.js
@@ -0,0 +1,11 @@
+import { EmptyError } from '../../error'
+import privs from './privs'
+
+function receive () {
+  const priv = privs.get(this)
+  if (!priv.messages.length) throw new EmptyError('no messages')
+  const message = priv.messages.shift()
+  return message
+}
+
+export default receive
---
+++ test/com/inbox/fifo/receive.js
@@ -0,0 +1,61 @@
+import test from 'ava'
+import Inbox from 'inbox/fifo/main'
+import add from 'inbox/fifo/add'
+import receive from 'inbox/fifo/receive'
+
+const message1 = 'Test message 1'
+const message2 = 'Test message 2'
+const message3 = 'Test message 3'
+
+test.before(t => {
+  Object.assign(Inbox.prototype, {
+    add,
+    receive
+  })
+})
+
+test('empty', t => {
+  const inbox = new Inbox()
+  t.throws(() => {
+    inbox.receive()
+  })
+})
+
+test('emptied', t => {
+  const inbox = new Inbox()
+  inbox.add(message1)
+  inbox.add(message2)
+  inbox.receive()
+  inbox.receive()
+  t.throws(() => {
+    inbox.receive()
+  })
+})
+
+test('1 message', t => {
+  const inbox = new Inbox()
+  inbox.add(message1)
+  const message = inbox.receive()
+  t.is(message, message1)
+})
+
+test('2 messages', t => {
+  const inbox = new Inbox()
+  inbox.add(message1)
+  inbox.add(message2)
+  t.is(inbox.receive(), message1)
+  t.is(inbox.receive(), message2)
+})
+
+test('add after receive', t => {
+  const inbox = new Inbox()
+  inbox.add(message1)
+  inbox.add(message2)
+  t.is(inbox.receive(), message1)
+  inbox.add(message3)
+  t.is(inbox.receive(), message2)
+  t.is(inbox.receive(), message3)
+  t.throws(() => {
+    inbox.receive()
+  })
+})
