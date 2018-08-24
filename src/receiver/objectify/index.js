eac7d4ba9175a40c7c489abd5bc3966ca4b28cc5
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-23 23:38:11 -0400

    Add receiver/objectify

---
+++ src/receiver/objectify/index.js
@@ -0,0 +1,15 @@
+/**
+ * Objectifying message receiver.
+ *
+ * Receives `protocol.Packet` instances.
+ * Converts to plain object.
+ * Passes on to next receiver.
+ *
+ * Provide protocol definition and next receiver.
+ *
+ * @module cashshuffle/receiver/objectify
+ */
+
+import Receiver from './main'
+
+export default Receiver
