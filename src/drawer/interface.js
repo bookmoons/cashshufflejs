66d72f3091d67614c89b49f8c894f83d08bf9de2
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-24 07:49:01 -0400

    Define Drawer

---
+++ src/drawer/interface.js
@@ -0,0 +1,34 @@
+/**
+ * Message drawer.
+ * @module cashshuffle/drawer
+ */
+
+/**
+ * Message drawer.
+ *
+ * Repeatedly draws protocol messages from an `Inchan`
+ * and relays them to a `Receiver`.
+ * Draws until stopped.
+ *
+ * @interface Drawer
+ */
+
+/**
+ * Start drawing.
+ *
+ * No effect if already drawing.
+ *
+ * @method start
+ * @memberof module:cashshuffle/drawer~Drawer
+ * @instance
+ */
+
+/**
+ * Stop drawing.
+ *
+ * No effect if already stopped.
+ *
+ * @method stop
+ * @memberof module:cashshuffle/drawer~Drawer
+ * @instance
+ */
