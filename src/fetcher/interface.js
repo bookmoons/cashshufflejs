05b3c6c5dd3301a4543c3b85655ae9ff5c9c2a4b
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-26 02:40:19 -0400

    Define Fetcher

---
+++ src/fetcher/interface.js
@@ -0,0 +1,26 @@
+/**
+ * Message fetcher.
+ * @module cashshuffle/fetcher
+ */
+
+/**
+ * Message fetcher.
+ *
+ * Fetches messages from `Inbox` instances.
+ * Details of fetching pattern implementation defined.
+ *
+ * @interface Fetcher
+ */
+
+/**
+ * Fetch messages.
+ *
+ * @method fetch
+ * @memberof module:cashshuffle/fetcher~Fetcher
+ * @instance
+ * @async
+ *
+ * @return Fetched messages. Structure implementation defined.
+ *
+ * @throws If fetching fails. Types and causes implementation defined.
+ */
