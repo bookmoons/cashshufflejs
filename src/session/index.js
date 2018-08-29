72d1158a4b44018a6a25e1b6d943423b71e60e65
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-08-29 05:59:05 -0400

    doc: Move Session description to class

--- src/session/index.js
+++ src/session/index.js
@@ -1,14 +1,4 @@
-/**
- * CoinShuffle session.
- *
- * CashShuffle is an extension to [CoinShuffle][1].
- * The CoinShuffle session is the core of the CashShuffle logic.
- * Executes a CoinShuffle session.
- *
- * [1]: http://crypsys.mmci.uni-saarland.de/projects/CoinShuffle/
- *
- * @module cashshuffle/session
- */
+/** @module cashshuffle/session */
 
 import Session from './main'
 import affix from './affix'
--- src/session/main.js
+++ src/session/main.js
@@ -25,6 +25,14 @@ import { defaultAttempts, defaultTimeout } from './default'
  */
 
 /**
+ * CoinShuffle session.
+ *
+ * CashShuffle is an extension to [CoinShuffle][1].
+ * The CoinShuffle session is the core of the CashShuffle logic.
+ * Executes a CoinShuffle session.
+ *
+ * [1]: http://crypsys.mmci.uni-saarland.de/projects/CoinShuffle/
+ *
  * @memberof module:cashshuffle/session
  */
 class Session {
