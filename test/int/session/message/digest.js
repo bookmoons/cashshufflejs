cc217adabc00853070ab466a8fbb82dffbd49d54
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-06 18:51:55 -0400

    test: Interpret hex string correctly

--- test/int/session/message/digest.js
+++ test/int/session/message/digest.js
@@ -12,7 +12,7 @@ const participantNumber = 100
 const signingPublicKey =
   '03f09e7bbaf09669b1cde3394db0b72c3408ed0826f98d7985a3cecc1486075d3b'
 const digestString = '0239480926758749870abdfefe'
-const digestBuffer = Buffer.from(digestString)
+const digestBuffer = Buffer.from(digestString, 'hex')
 const digest = toArrayBuffer(digestBuffer)
 const digestView = new Uint8Array(digest)
 const expectedHashObject = { hash: digestView }
