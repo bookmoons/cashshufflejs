ef8bbd29ce2de35a4361d369eaddefcbada0cc08
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-11 13:50:30 -0400

    coin/bc#verifySignature: Add param network

--- src/coin/bitcore/verify.js
+++ src/coin/bitcore/verify.js
@@ -1,8 +1,15 @@
 import bitcore from 'bitcore-lib-cash'
 import Message from '@bookmoons/bitcore-message-cash'
 
-async function verifySignature (signature, message, publicKeyString) {
-  const publicKey = new bitcore.PublicKey(publicKeyString)
+const mainnet = bitcore.Networks.mainnet
+
+async function verifySignature (
+  signature,
+  message,
+  publicKeyString,
+  network = mainnet
+) {
+  const publicKey = new bitcore.PublicKey(publicKeyString, { network })
   const address = publicKey.toAddress()
   const messageObject = new Message(message)
   const valid = messageObject.verify(address, signature)
