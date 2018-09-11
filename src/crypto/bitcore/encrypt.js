ecaa79c18bea4e4be0801edb268a8aa49f213b4c
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-11 14:06:10 -0400

    crypto/bc#encrypt: Add param network

--- src/crypto/bitcore/encrypt.js
+++ src/crypto/bitcore/encrypt.js
@@ -1,11 +1,13 @@
 import bitcore from 'bitcore-lib-cash'
 import ECIES from '@bookmoons/bitcore-ecies-cash'
 
-async function encrypt (message, recipient) {
-  const ephemeralPrivateKey = new bitcore.PrivateKey()
+const mainnet = bitcore.Networks.mainnet
+
+async function encrypt (message, recipient, network = mainnet) {
+  const ephemeralPrivateKey = new bitcore.PrivateKey(null, network)
   const encryptor = new ECIES()
   encryptor.privateKey(ephemeralPrivateKey)
-  const recipientPublicKey = new bitcore.PublicKey(recipient)
+  const recipientPublicKey = new bitcore.PublicKey(recipient, { network })
   encryptor.publicKey(recipientPublicKey)
   const cryptogramBuffer = encryptor.encrypt(message)
   const cryptogramBase64 = cryptogramBuffer.toString('base64')
