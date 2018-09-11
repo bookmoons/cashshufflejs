7bf81566f58f9c11536e71a311a22f789c9b7139
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-11 13:56:23 -0400

    coin/bc: Define network type

--- src/coin/bitcore/index.js
+++ src/coin/bitcore/index.js
@@ -6,6 +6,9 @@
  * - [bitcore-lib-cash][2] for key operations.
  * - [&#64;bookmoons/bitcore-message-cash][3] for message verification.
  *
+ * Network values are bitcore-lib-cash `Network` instances. Standard networks
+ * are available through `bitcore.Networks`.
+ *
  * [1]: https://bitcore.io/
  * [2]: https://www.npmjs.com/package/bitcore-lib-cash
  * [3]: https://www.npmjs.com/package/@bookmoons/bitcore-message-cash
