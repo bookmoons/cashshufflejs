063969caa901a757e5a05a457ebf401399aee352
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-11 14:47:46 -0400

    rcv/auth: Correct network reference

--- src/receiver/authenticate/submit.js
+++ src/receiver/authenticate/submit.js
@@ -66,7 +66,7 @@ async function submit (message) {
     senderPublicKeyString,
     { network: priv.network }
   )
-  const senderAddress = senderPublicKey.toAddress(network)
+  const senderAddress = senderPublicKey.toAddress(priv.network)
   // Normalize to buffer. protobufjs can return Uint8Array Buffer Array.
   const signatureBuffer = Buffer.from(signatureValue)
   const signatureString = signatureBuffer.toString('utf8')
