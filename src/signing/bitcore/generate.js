55630b93e878e65bd610c9c5c92797131a8ae6f9
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-11 14:28:52 -0400

    signing/bc: Specify network for bitcore constructs

--- src/signing/bitcore/generate.js
+++ src/signing/bitcore/generate.js
@@ -6,8 +6,8 @@ const mainnet = bitcore.Networks.mainnet
 async function generateKeyPair (network = mainnet) {
   const priv = privs.get(this)
   const privateKey = new bitcore.PrivateKey(null, network)
-  const publicKey = new bitcore.PublicKey(privateKey)
-  const address = new bitcore.Address(publicKey)
+  const publicKey = new bitcore.PublicKey(privateKey, { network })
+  const address = new bitcore.Address(publicKey, network)
   const keyPair = {
     privateKey,
     publicKey,
