eb79fb792842557865ad5b765efc4010972a20f0
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-12 20:11:00 -0400

    test: Add spy logchan

---
+++ test/spy/logchan.js
@@ -0,0 +1,16 @@
+import sinon from 'sinon'
+import DummyLogchan from '../dummy/logchan'
+
+class SpyLogchan extends DummyLogchan {}
+
+const stringKeys = Object.getOwnPropertyNames(DummyLogchan.prototype)
+const symbolKeys = Object.getOwnPropertySymbols(DummyLogchan.prototype)
+const keys = [ ...stringKeys, ...symbolKeys ]
+for (const key of keys) {
+  const member = DummyLogchan.prototype[key]
+  if (typeof member !== 'function') continue
+  const spy = sinon.spy(member)
+  SpyLogchan.prototype[key] = spy
+}
+
+export default SpyLogchan
