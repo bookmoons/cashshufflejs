7383c99a3f1bf9e9f89871a6b3e6ab98403eb2d8
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-13 13:32:42 -0400

    protocol: Simplify module description
    Moving protocol details documentation into the doc section.

--- src/protocol/index.js
+++ src/protocol/index.js
@@ -1,11 +1,5 @@
 /**
- * Protocol details. Message types with [protobuf][1].
- *
- * Messages on the wire are terminated with the Unicode character U+23CE
- * Return Symbol in UTF-8. The byte encoding is e2 8f 8e.
- *
- * [1]: https://developers.google.com/protocol-buffers/
- *
+ * Protocol details.
  * @module cashshuffle/protocol
  */
 
