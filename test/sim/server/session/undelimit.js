cc912160c7688f2cf58b15b00ef4aec7a708c676
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-15 18:30:17 -0400

    serv/sess: Implement undelimit

---
+++ test/sim/server/session/undelimit.js
@@ -0,0 +1,40 @@
+import { ValueError } from 'error'
+import { terminatorBuffer, terminatorByteLength } from 'protocol'
+
+/**
+ * @typedef {Array} UndelimitReturn
+ * @memberof module:cashshuffle/sim/server/session.SessionServerSimulator
+ *
+ * @prop {Buffer} 1 - Undelimited message.
+ * @prop {Buffer} 2 - Data chunk with undelimited message removed.
+ *
+ * @example
+const message = Buffer.from([ 0x01, 0x02, 0x03 ])
+const newData = Buffer.from([ 0x04, 0x05 ])
+const undelimitReturn = [ message, newData ]
+ */
+
+/**
+ * Undelimit a single message.
+ *
+ * Extracts a single delimited message from a binary data chunk.
+ * Extracts from the front of the chunk.
+ * Extracted message delivered without delimiter.
+ *
+ * @param {Buffer} data - Data chunk to undelimit from.
+ *
+ * @return {UndelimitReturn} The undelimited message and new data chunk.
+ *
+ * @throws {ValueError} If data chunk contains no valid delimited message.
+ *     Message `'no delimited message'`.
+ */
+function undelimit (data) {
+  const index = data.indexOf(terminatorBuffer)
+  if (index === -1) throw new ValueError('no delimited message')
+  const message = data.slice(0, index)
+  const newDataIndex = index + terminatorByteLength
+  const newData = data.slice(newDataIndex)
+  return [ message, newData ]
+}
+
+export default undelimit
---
+++ test/unit/sim/server/session/undelimit.js
@@ -0,0 +1,58 @@
+import test from 'ava'
+import { ValueError } from 'error'
+import { terminatorBuffer } from 'protocol'
+import undelimit from 'sim/server/session/undelimit'
+
+const testMessage1 = Buffer.from([ 0x01, 0x02, 0x03 ])
+const testMessage2 = Buffer.from([ 0x04, 0x05, 0x06 ])
+const emptyData = Buffer.alloc(0)
+const noMessageData = Buffer.from([ 0x01 ])
+const emptyMessageData = Buffer.concat([ terminatorBuffer ])
+const oneMessageData = Buffer.concat([ testMessage1, terminatorBuffer ])
+const twoMessagesData = Buffer.concat([
+  testMessage1, terminatorBuffer,
+  testMessage2, terminatorBuffer
+])
+
+test('empty data', t => {
+  try {
+    undelimit(emptyData)
+    t.fail('Incorrect success')
+  } catch (e) {
+    t.true(e instanceof ValueError)
+    t.is(e.message, 'no delimited message')
+  }
+})
+
+test('no message', t => {
+  try {
+    undelimit(noMessageData)
+    t.fail('Incorrect success')
+  } catch (e) {
+    t.true(e instanceof ValueError)
+    t.is(e.message, 'no delimited message')
+  }
+})
+
+test('empty message', t => {
+  const [ message ] = undelimit(emptyMessageData)
+  t.is(message.length, 0)
+})
+
+test('1 message', t => {
+  const [ message, newData ] = undelimit(oneMessageData)
+  t.deepEqual(message, testMessage1)
+  t.throws(() => {
+    undelimit(newData)
+  })
+})
+
+test('2 messages', t => {
+  const [ message1, newData1 ] = undelimit(twoMessagesData)
+  t.deepEqual(message1, testMessage1)
+  const [ message2, newData2 ] = undelimit(newData1)
+  t.deepEqual(message2, testMessage2)
+  t.throws(() => {
+    undelimit(newData2)
+  })
+})
