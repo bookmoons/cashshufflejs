80d203752f851fc3c8e8f807d822e70fb7143fed
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-14 23:53:12 -0400

    sim/sockend: Deliver errors to callback
    Recommended in the Node.js docs.

--- src/sim/socketend/write.js
+++ src/sim/socketend/write.js
@@ -4,9 +4,13 @@ function _write (chunk, encoding, callback) {
   const priv = privs.get(this)
   const writeHandler = priv.writeHandler
   const writeHandlerDone = writeHandler(chunk)
-  writeHandlerDone.then(function handleWriteHandlerDone () {
-    callback()
-  })
+  writeHandlerDone
+    .then(function handleWriteHandlerDone () {
+      callback()
+    })
+    .catch(function handleWriteHandlerError (error) {
+      callback(error)
+    })
 }
 
 export default _write
