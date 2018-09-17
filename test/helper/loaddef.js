301f7e6922e8dcd2e4cc4cf26b907054c20322d6
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-17 01:02:20 -0400

    test: Add loadDefault

---
+++ test/helper/loaddef.js
@@ -0,0 +1,16 @@
+import loadModule from './loadmod'
+
+/**
+ * Dynamically load the default export of a module.
+ *
+ * @param {string} path - Module path.
+ *
+ * @return The default export of the loaded module.
+ */
+async function loadDefault (path) {
+  const module = await loadModule(path)
+  const defaultExport = module.default
+  return defaultExport
+}
+
+export default loadDefault
