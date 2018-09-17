e0352205ee61972503f67058f6fa716d283ff86b
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-17 14:59:38 -0400

    util: Qualify nodeBufferToBytes param name

--- src/util/tobytes/nodebuffer.js
+++ src/util/tobytes/nodebuffer.js
@@ -6,14 +6,14 @@
 /**
  * Convert `Buffer` to `Uint8Array`.
  *
- * @param {Buffer} buffer - `Buffer` to convert.
+ * @param {Buffer} nodeBuffer - `Buffer` to convert.
  *
  * @return {Uint8Array} New `Uint8Array` containing copy of `buffer`.
  */
-function nodeBufferToBytes (buffer) {
-  const length = buffer.length
+function nodeBufferToBytes (nodeBuffer) {
+  const length = nodeBuffer.length
   const bytes = new Uint8Array(length)
-  for (let i = 0; i < length; i++) bytes[i] = buffer[i]
+  for (let i = 0; i < length; i++) bytes[i] = nodeBuffer[i]
   return bytes
 }
 
