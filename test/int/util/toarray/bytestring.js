919386c892ccf3cf4ec47deefb8480c27d5b9b86
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-17 21:11:50 -0400

    util.byteStringToArray: Add integration tests

---
+++ test/int/util/toarray/bytestring.js
@@ -0,0 +1,35 @@
+import test from 'ava'
+import { FormatError } from 'error'
+import byteStringToArray from 'util/toarray/bytestring'
+
+test('invalid', t => {
+  const byteString = String.fromCodePoint(888)
+  const error = t.throws(() => {
+    byteStringToArray(byteString)
+  })
+  t.true(error instanceof FormatError)
+  t.true(error.message.startsWith('invalid byte string'))
+})
+
+test('empty', t => {
+  const bytesArray = byteStringToArray('')
+  t.deepEqual(bytesArray, [])
+})
+
+test('valid', t => {
+  const codePoints = []
+  for (let i = 0; i <= 255; i++) codePoints.push(i)
+  const byteString = codePoints.reduce(
+    function reduceCodePoint (byteString, codePoint) {
+      const character = String.fromCodePoint(codePoint)
+      byteString += character
+      return byteString
+    },
+    ''
+  )
+  const bytesArray = byteStringToArray(byteString)
+  t.is(bytesArray.length, codePoints.length)
+  for (let i = 0; i < bytesArray.length; i++) {
+    t.is(bytesArray[i], codePoints[i])
+  }
+})
