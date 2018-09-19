955ff558d9506fc35e27a869a59a8fc3f4f3591c
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-09-19 16:08:41 -0400

    rcv/submit: Remove erroneous #submit documentation
    Seemingly meant for the discarder.

--- src/receiver/store/submit.js
+++ src/receiver/store/submit.js
@@ -1,10 +1,5 @@
 import privs from './privs'
 
-/**
- * @memberof module:cashshuffle/receiver/store.StoreReceiver
- *
- * @param {Array<Error,*>} message - Discard reason and discarded message.
- */
 async function submit (message) {
   const priv = privs.get(this)
   priv.inbox.add(message)
