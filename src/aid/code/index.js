e29e87f0f799f5ec7e1371aa3c998d72949b4f0c
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-11-24 08:03:20 -0500

    aid/code: Expose crypt/string

--- src/aid/code/index.js
+++ src/aid/code/index.js
@@ -2,3 +2,6 @@
  * Coding procedures.
  * @module cashshuffle/aid/code
  */
+
+export { default as cryptDecodeString } from './crypt/string/decode'
+export { default as cryptEncodeString } from './crypt/string/encode'
