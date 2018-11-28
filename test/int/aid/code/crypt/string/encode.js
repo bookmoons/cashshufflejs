37ef371475ef3bd296b435e60f46f2f6ab9a5c94
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-11-28 09:26:20 -0500

    aid/code#cryptEncodeString@test: Use descriptive variable names

--- test/int/aid/code/crypt/string/encode.js
+++ test/int/aid/code/crypt/string/encode.js
@@ -2,22 +2,22 @@ import test from 'ava'
 import cryptEncodeString from 'aid/code/crypt/string/encode'
 
 test('bytes', t => {
-  const bytes = cryptEncodeString('Message')
-  t.true(bytes instanceof Uint8Array)
+  const encoded = cryptEncodeString('Message')
+  t.true(encoded instanceof Uint8Array)
 })
 
 test('empty', t => {
-  const bytes = cryptEncodeString('')
-  t.deepEqual(bytes, new Uint8Array(0))
+  const encoded = cryptEncodeString('')
+  t.deepEqual(encoded, new Uint8Array(0))
 })
 
 test('ascii', t => {
-  const bytes = cryptEncodeString('abc')
-  t.deepEqual(bytes, Uint8Array.from([ 0x61, 0x62, 0x63 ]))
+  const encoded = cryptEncodeString('abc')
+  t.deepEqual(encoded, Uint8Array.from([ 0x61, 0x62, 0x63 ]))
 })
 
 test('high char', t => {
-  const string = String.fromCodePoint(9829) // '♥'
-  const bytes = cryptEncodeString(string)
-  t.deepEqual(bytes, Uint8Array.from([ 0xe2, 0x99, 0xa5 ]))
+  const message = String.fromCodePoint(9829) // '♥'
+  const encoded = cryptEncodeString(message)
+  t.deepEqual(encoded, Uint8Array.from([ 0xe2, 0x99, 0xa5 ]))
 })
