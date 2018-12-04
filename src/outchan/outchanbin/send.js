c6c210b66a4360894704814f9c39e0f67a15e76a
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-12-04 13:18:55 -0500

    Centralize message transfer encoding

--- src/outchan/outchanbin/send.js
+++ src/outchan/outchanbin/send.js
@@ -1,14 +1,13 @@
 import { BusyError } from '/error'
-import { normalizeProtobufBytes } from '/aid/normalize'
+import { transferEncodePackets } from '/aid/code'
 import privs from './privs'
 
 async function send (message) {
   const priv = privs.get(this)
   if (priv.sending) throw new BusyError('Another send call is running')
   priv.sending = true
-  const messageBytesDenormal = priv.protocol.Packets.encode(message).finish()
-  const messageBytes = normalizeProtobufBytes(messageBytesDenormal)
-  await priv.outchanbin.send(messageBytes)
+  const messageEncoded = transferEncodePackets(priv.protocol, message)
+  await priv.outchanbin.send(messageEncoded)
   priv.sending = false
 }
 
